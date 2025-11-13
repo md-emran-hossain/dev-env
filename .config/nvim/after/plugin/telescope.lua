5ef5ffa5d7b7c22e9c78eb4e13ad194288e2a134
Md Emran Hossain <69568149+md-emran-hossain@users.noreply.github.com>
2025-11-13 18:53:19 +0600

    added ignoring pattern for file search

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -2,10 +2,21 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', function()
 	builtin.find_files({
 		hidden = true,
-		file_ignore_patterns = { "^node_modules/", "^%.git/" }
+		no_ignore = true,
+		file_ignore_patterns = { "node_modules/.*", "%.git/.*" },
 	})
 end, { desc = 'Telescope find/project files' })
 vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope git files' })
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
+  builtin.grep_string({
+    search = vim.fn.input("Grep > "),
+    additional_args = function()
+      return {
+        "--hidden",
+        "--no-ignore",
+        "--glob", "!**/node_modules/*",
+        "--glob", "!**/.git/*"
+      }
+    end,
+  })
+end, { desc = 'Telescope grep project files' })
