38a73461f654848a65c9a9012cc0cbacaed3ca9b
Md Emran Hossain <69568149+md-emran-hossain@users.noreply.github.com>
2025-12-13 23:04:06 +0600

    apply formatting by shorcut rather than on write time

--- .config/nvim/after/plugin/conform.lua
+++ .config/nvim/after/plugin/conform.lua
@@ -1,4 +1,6 @@
-require("conform").setup({
+local conform = require("conform")
+
+conform.setup({
 	formatters_by_ft = {
 		javascript = { "prettier" },
 		javascriptreact = { "prettier" },
@@ -13,14 +15,9 @@ require("conform").setup({
 	},
 })
 
--- Auto-format on save
-vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*",
-	callback = function()
-		require("conform").format({
-			async = false,
-			lsp_fallback = true, -- use LSP formatter if Prettier is not configured
-		})
-	end,
-})
-
+vim.keymap.set({ "n", "v" }, "<leader>f", function()
+	conform.format({
+		async = false,
+		lsp_fallback = true, -- use LSP formatter if Prettier is not configured
+	})
+end, { desc = "Format file/selection" })
