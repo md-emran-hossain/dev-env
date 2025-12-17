59866bccbee304e29dc5c714e300fbca92d644fb
Md Emran Hossain <69568149+md-emran-hossain@users.noreply.github.com>
2025-12-17 12:38:29 +0600

    updated the diagonistic shortcut

--- .config/nvim/after/plugin/trouble.lua
+++ .config/nvim/after/plugin/trouble.lua
@@ -10,8 +10,8 @@ require("trouble").setup {
 
 local map = vim.keymap.set
 
-map("n", "<leader>pw", "<cmd>Trouble diagnostics toggle<CR>", { desc = "Workspace Diagnostics" })
-map("n", "<leader>pd", "<cmd>Trouble diagnostics toggle filter.buf=0 focus=true<CR>", { desc = "Document Diagnostics" })
-map("n", "<leader>pq", "<cmd>Trouble qflist toggle focus=true<CR>", { desc = "Quickfix List" })
-map("n", "<leader>pl", "<cmd>Trouble loclist toggle focus=true<CR>", { desc = "Location List" })
+map("n", "<leader>dw", "<cmd>Trouble diagnostics toggle<CR>", { desc = "Workspace Diagnostics" })
+map("n", "<leader>dd", "<cmd>Trouble diagnostics toggle filter.buf=0 focus=true<CR>", { desc = "Document Diagnostics" })
+map("n", "<leader>dq", "<cmd>Trouble qflist toggle focus=true<CR>", { desc = "Quickfix List" })
+map("n", "<leader>dl", "<cmd>Trouble loclist toggle focus=true<CR>", { desc = "Location List" })
 -- map("n", "<leader>xt", "<cmd>Trouble todo toggle focus=true<CR>", { desc = "TODOs" })
