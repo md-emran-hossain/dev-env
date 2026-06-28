9cc0746a9066cfb82fdcf38d459361f3f46ada37
Md Emran Hossain <69568149+md-emran-hossain@users.noreply.github.com>
2026-06-28 23:52:43 +0600

    style: customize indent-blankline for rose-pine theme

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -1,5 +1,21 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
 	main = "ibl",
-	opts = {}
+	config = function()
+		-- Rose Pine's "muted" color! Used for code comments
+		vim.api.nvim_set_hl(0, "IblScopeCustom", { fg = "#6e6a86", nocombine = true })
+
+		require("ibl").setup({
+			indent = {
+				char = "▏",
+			},
+			scope = {
+				-- Apply your custom subtle color
+				highlight = "IblScopeCustom",
+				-- Disable the underlines at the top/bottom of the scope block
+				show_start = false,
+				show_end = false,
+			}
+		})
+	end
 }
